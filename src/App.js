87c8bb75648bb1835be4150771d71f27b0f4e246
Bezzubenko Viacheslav <bezzubenko_slava@mail.ru>
2024-07-29 17:42:50 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      const res = await fetch('http://localhost:8080/translation/word', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          text: inputValue,
+          source: 'ENGLISH',
+          destination: 'RUSSIAN'
+        }),
+      });
+      const data = await res.text(); // Assuming the response is plain text
+      setResponse(data);
+    } catch (error) {
+      console.error('Error:', error);
+      setResponse('Something went wrong.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Easy dictionary app</h1>
+          <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Enter word to translate"
+          />
+          <button onClick={handleButtonClick}>Translate</button>
+          <p>Response: {response}</p>
+        </header>
+      </div>
   );
 }
 
